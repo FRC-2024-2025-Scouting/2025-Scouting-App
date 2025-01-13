4087a9d41fc8e157acc4e6b9ca181a42e04258b2
Harrison-Furg <1fergusonhar@hdsb.ca>
2025-01-13 06:13:47 -0800

    Added variables and started button functionality

--- src/Auto.js
+++ src/Auto.js
@@ -18,31 +18,39 @@ export function AutoPage() {
     );
   }
 
+function Tally(vlair) {
+    const [vlair, setCount] = useState(0)
+
+    const increment = () => setCount(vlair + 1);
+    const deincrement = () => {if (vlair!=0)
+        {setCount(count -1);} };
+    return(
+        <div class="row">
+            <button class="autoButton" onClick={deincrement}> &lt; </button>
+            <div class="autoCounter">{vlair}</div>
+            <buddon class="autoButton" onClick={increment}> &gt; </buddon>
+        </div>
+
+    )
+}
 
 function Score() {
     return(
         <div class="column">
             <div class="head">Score</div>
 
-            <div class="row">            
-                <button class="autoButton"> &lt; </button>
-                <div class="autoCounter">0</div>
-                <button class="autoButton"> &gt; </button></div>
-            <div class="row">
-                <button class="autoButton"> &lt; </button>
-                <div class="autoCounter">0</div>
-                <button class="autoButton"> &gt; </button>
-            </div>
-            <div class="row">
-                <button class="autoButton"> &lt; </button>
-                <div class="autoCounter">0</div>
-                <button class="autoButton"> &gt; </button>
-            </div>
-            <div class="row">
-                <button class="autoButton"> &lt; </button>
-                <div class="autoCounter">0</div>
-                <button class="autoButton"> &gt; </button>
-            </div>
+            <Tally
+            vlair={AL1S}
+            />
+            <Tally
+            vlair={AL2S}
+            />
+            <Tally
+            vlair={AL3S}
+            />
+            <Tally
+            vlair={AL4S}
+            />
         </div>
     );
 }
---
+++ src/Variables.js
@@ -0,0 +1,54 @@
+// Pregame
+let matchNum = 0; //#
+let teamNum = 0; //#
+let colour = 0; //#-restricted
+
+// Auto
+let moved; //bool
+let AL1S = 0; //# Layer 1 score
+let AL2S = 0; //# Layer 2 score
+let AL3S = 0; //# Layer 3 score
+let AL4S = 0; //# Layer 4 score
+
+let AL1M = 0; //# Layer 1 Miss
+let AL2M = 0; //# Layer 2 Miss
+let AL3M = 0; //# Layer 3 Miss
+let AL4M = 0; //# Layer 4 Miss
+
+let APS = 0; //# Processor Score
+let APM = 0; //# Processor Miss
+
+let ANS = 0; //# Net Score
+let ANM = 0; //# Net Miss
+
+//Tele
+let TL1S = 0; //# Layer 1 score
+let TL2S = 0; //# Layer 2 score
+let TL3S = 0; //# Layer 3 score
+let TL4S = 0; //# Layer 4 score
+
+let TL1M = 0; //# Layer 1 Miss
+let TL2M = 0; //# Layer 2 Miss
+let TL3M = 0; //# Layer 3 Miss
+let TL4M = 0; //# Layer 4 Miss
+
+let TPS = 0; //# Processor Score
+let TPM = 0; //# Processor Miss
+
+let TNS = 0; //# Net Score
+let TNM = 0; //# Net Miss
+
+let BRKDN = 0; //# Breakdowns
+let RCVR = 0; //# Recoveries
+
+//End
+let PRK = false; //Bool Pooked
+
+let SCC = 0; //# Shallow Cage Climb
+let SCF = 0; //# Shallow Cage Fall
+
+let DCC = 0; //# Deep Cage Climb
+let DCF = 0; //# Deep Cage Climb
+
+let Cards = 0; //#-restricted 
+let Fouls = 0; //#
